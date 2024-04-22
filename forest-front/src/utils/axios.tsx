729868ee1077c75a65806553d183060a8f1fc9cd
titi0267 <timothe@coniel.com>
2024-04-23 02:46:25 +0800

    idk

--- forest-front/src/utils/axios.tsx
+++ forest-front/src/utils/axios.tsx
@@ -1,14 +1,15 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:80/tree',
+  baseURL: "http://localhost:80/tree",
 });
 
-axiosInstance.interceptors.request.use(config => {
-  config.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000';
-  config.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
-  config.headers['Access-Control-Allow-Headers'] = 'Content-Type';
-  config.headers['Access-Control-Allow-Credentials'] = true;
+axiosInstance.interceptors.request.use((config) => {
+  config.headers["Access-Control-Allow-Origin"] = "http://localhost:3000";
+  config.headers["Access-Control-Allow-Methods"] =
+    "GET, POST, PUT, DELETE, OPTIONS";
+  config.headers["Access-Control-Allow-Headers"] = "Content-Type";
+  config.headers["Access-Control-Allow-Credentials"] = true;
   return config;
 });
 
---
+++ forest-front/src/utils/data/StandTable.ts
@@ -0,0 +1,8 @@
+export const standTable = [
+  'SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";',
+  "START TRANSACTION;",
+  'SET time_zone = "+00:00";',
+  "CREATE TABLE `StandTable` (    `GroupName` text NOT NULL,    `Diameter` float NOT NULL,    `Volume` float NOT NULL,    `Num` int(11) NOT NULL  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;",
+  "INSERT INTO `StandTable` (`GroupName`, `Diameter`, `Volume`, `Num`) VALUES  ('Mersawa', 1, 142.227, 1500),  ('Mersawa', 2, 567.535, 1199),  ('Mersawa', 3, 504.684, 401),  ('Mersawa', 4, 497.459, 200),  ('Mersawa', 5, 4841.69, 200),  ('Keruing', 1, 204.283, 2097),  ('Keruing', 2, 844.209, 1800),  ('Keruing', 3, 761.486, 603),  ('Keruing', 4, 978.306, 399),  ('Keruing', 5, 9422.33, 401),  ('Dip Commercial', 1, 200.696, 2099),  ('Dip Commercial', 2, 826.005, 1799),  ('Dip Commercial', 3, 765.434, 600),  ('Dip Commercial', 4, 973.393, 401),  ('Dip Commercial', 5, 10296.1, 401),  ('Dip NonCommercial', 1, 291.047, 2996),  ('Dip NonCommercial', 2, 1239.02, 2703),  ('Dip NonCommercial', 3, 1116.08, 901),  ('Dip NonCommercial', 4, 1210.3, 500),  ('Dip NonCommercial', 5, 8021.95, 300),  ('NonDip Commercial', 1, 287.639, 2997),  ('NonDip Commercial', 2, 1245.22, 2701),  ('NonDip Commercial', 3, 1118.85, 901),  ('NonDip Commercial', 4, 962.509, 400),  ('NonDip Commercial', 5, 9559.91, 401),  ('NonDip NonCommercial', 1, 373.975, 3899),  ('NonDip NonCommercial', 2, 1679.33, 3596),  ('NonDip NonCommercial', 3, 1505.13, 1204),  ('NonDip NonCommercial', 4, 1749.67, 701),  ('NonDip NonCommercial', 5, 10244.2, 400),  ('Others', 1, 422.869, 4396),  ('Others', 2, 1932.79, 4201),  ('Others', 3, 1787.85, 1403),  ('Others', 4, 2187.53, 899),  ('Others', 5, 9646.34, 401);",
+  "COMMIT;",
+];
